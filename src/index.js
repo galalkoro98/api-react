80d675346d09856797015840e49407751aa36d1c
galalkoro98 <galal.koro@gmail.com>
2023-02-23 00:12:36 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import ReactDOM from "react-dom/client";
 
+import "./styles/font.css";
+import "./styles/general.css";
+
 import { App } from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
